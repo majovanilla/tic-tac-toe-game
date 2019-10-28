c9125fddca869e5a118e744b85cb9244a305aa89
majovainilla <maria_reyes@me.com>
2019-10-28 12:01:15 -0600

    CREATE GAME AND PLAYER CLASSES

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -15,6 +15,21 @@
 # Optional methods to check rows, columns and diagonals
 # Method restart
 
+class Game
+  def initialize()
+    @board = { a1: "\s", a2: "\s", a3: "\s", b1: "\s", b2: "\s", b3: "\s", c1: "\s", c2: "\s", c3: "\s" }
+  end
+end
+
+class Player
+	@@player_number = 0 
+	def initialize(name)
+		@name = name
+		@player_char = @@pl
+		@@player_number += 1
+  end
+end
+
 
 # Player(name = player variable)
 # @name
