904d4d9d76e62ee522cb8410377880bb8d0736c5
majovainilla <maria_reyes@me.com>
2019-10-28 11:52:09 -0600

    ADD GENERAL LOGIC COMMENTS

---
+++ lib/game_logic.rb
@@ -0,0 +1,23 @@
+#classes:
+
+# Game
+# Could include a class variable with games played
+# Method / call to class to display the board (instead of puts on line 18)
+# Method to instatiate the game / start game method
+# Method to create a new player takes as a parameter the player name variable
+# Method that includes the loop
+# Method for quitting game
+# Method check_choice
+# Method house_empty?
+# Method choice_valid?
+# Method update_board
+# Method winner
+# Optional methods to check rows, columns and diagonals
+# Method restart
+
+
+# Player(name = player variable)
+# @name
+# @player_char
+
+#
