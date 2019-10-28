ed30de86cf7c2f49a276026c4e39f815f678a99c
sarah <sarachamorro@gmail.com>
2019-10-28 13:13:16 -0300

    ADD CHECK FOR ALREADY FULL HOUSE

--- bin/main.rb
+++ bin/main.rb
@@ -7,9 +7,9 @@ houses = { a1: "\s", a2: "\s", a3: "\s", b1: "\s", b2: "\s", b3: "\s", c1: "\s",
 puts "Game Start!\n"
 
 puts "Input player 1 name:\n"
-player1 = gets.chomp
+player1 = gets.chomp.capitalize
 puts "Input player 2 name:\n"
-player2 = gets.chomp
+player2 = gets.chomp.capitalize
 
 puts "Tic-tac-toe is a very fun game!\n#{player1} gets the 'X' and #{player2} the 'O'\nThe first one to connect three X's O's sequence is the Winner!\n"
 
@@ -22,17 +22,43 @@ turns = 1
 while turns <= 9
   if turns.odd?
     puts "#{player1} it's your turn! Choose a house!"
-    player1_choice = gets.chomp
+    player1_choice = gets.chomp.downcase
+    if player1_choice == 'quit'
+      puts 'Quitting game'
+      break
+    end
     player1_choice = player1_choice.to_sym
-    redo if houses[player1_choice].nil?
+    if houses[player1_choice].nil?
+      puts ' Please enter a valid input.'
+      puts "\n HINT: combine the letters a,b,c with the numbers 1,2,3 like 'a2'"
+      redo
+    end
+    if houses[player1_choice].match(/[OX]/)
+      puts ' Please select an empty house'
+      redo
+    end
     houses[player1_choice] = 'O'
   else
     puts "#{player2} it's your turn! Choose a house!"
-    player2_choice = gets.chomp
+    player2_choice = gets.chomp.downcase
+    if player2_choice == 'quit'
+      puts 'Quitting game'
+      break
+    end
     player2_choice = player2_choice.to_sym
-    redo if houses[player2_choice].nil?
+    if houses[player2_choice].nil?
+      puts ' Please enter a valid input.'
+      puts "\n HINT: combine the letters a,b,c with the numbers 1,2,3 like 'a2'"
+      redo
+    end
+    if houses[player2_choice].match(/[OX]/)
+      puts ' Please select an empty house'
+      redo
+    end
     houses[player2_choice] = 'X'
   end
   puts "    1  2  3\n a [#{houses[:a1]}][#{houses[:a2]}][#{houses[:a3]}]\n b [#{houses[:b1]}][#{houses[:b2]}][#{houses[:b3]}]\n c [#{houses[:c1]}][#{houses[:c2]}][#{houses[:c3]}]\n"
   turns += 1
+
+  puts "End of game"
 end
