c0a7fa8fd54255385ed8b7f40fdaa51eb33bc9e7
sarah <sarachamorro@gmail.com>
2019-10-28 12:11:11 -0300

    ADD PLAYING LOOP

--- bin/main.rb
+++ bin/main.rb
@@ -1,22 +1,35 @@
 # !/usr/bin/env ruby
 
-houses = { "a1": "\s", "a2": "\s", "a3": "\s", "b1": "\s", "b2": "\s", "b3": "\s", "c1": "\s", "c2": "\s", "c3": "\s"}
-a1 = "_"
+houses = { a1: "\s", a2: "\s", a3: "\s", b1: "\s", b2: "\s", b3: "\s", c1: "\s", c2: "\s", c3: "\s"}
 
 puts "Game Start!\n"
 
 puts "Input player 1 name:\n"
 player1 = gets.chomp
-puts "Input player 1 name:\n"
+puts "Input player 2 name:\n"
 player2 = gets.chomp
 
 puts "Tic-tac-toe is a very fun game!\n#{player1} gets the 'X' and #{player2} the 'O'\nThe first one to connect three X's O's sequence is the Winner!\n"
 
 puts "Here's your Tic-tac-toe game board!\n"
 
-puts "    1  2  3\n a [#{houses["a1"]}][#{houses["a2"]}][#{houses["a3"]}]\n b [#{houses["b1"]}][#{houses["b2"]}][#{houses["b3"]}]\n c [#{houses["c1"]}][#{houses["c2"]}][#{houses["c3"]}]\n"
+puts "    1  2  3\n a [#{houses[:a1]}][#{houses[:a2]}][#{houses[:a3]}]\n b [#{houses[:b1]}][#{houses[:b2]}][#{houses[:b3]}]\n c [#{houses[:c1]}][#{houses[:c2]}][#{houses[:c3]}]\n"
 
-puts "Choose a house from the board (choose a letter and a number according to the board above)"
+turns = 1;
 
-puts "#{player1} it's your turn!"
-player1_choice = gets.chomp
+while turns <= 5
+    puts "#{player1} it's your turn! Choose a house!"
+    player1_choice = gets.chomp
+    player1_choice = player1_choice.to_sym
+    houses[player1_choice] = "O"
+    puts "    1  2  3\n a [#{houses[:a1]}][#{houses[:a2]}][#{houses[:a3]}]\n b [#{houses[:b1]}][#{houses[:b2]}][#{houses[:b3]}]\n c [#{houses[:c1]}][#{houses[:c2]}][#{houses[:c3]}]\n"
+
+    unless turns == 5
+      puts "#{player2} it's your turn! Choose a house!"
+      player2_choice = gets.chomp
+      player2_choice = player2_choice.to_sym
+      houses[player2_choice] = "X"
+      puts "    1  2  3\n a [#{houses[:a1]}][#{houses[:a2]}][#{houses[:a3]}]\n b [#{houses[:b1]}][#{houses[:b2]}][#{houses[:b3]}]\n c [#{houses[:c1]}][#{houses[:c2]}][#{houses[:c3]}]\n"
+    end
+    turns += 1
+end
