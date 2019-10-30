d3a6ca0645bc976a64594e9264858a5246acd428
sarah <sarachamorro@gmail.com>
2019-10-30 08:09:03 -0300

    EDIT PROMPTS SO THAT PRESSING ENTER IS EXPLICIT

--- bin/main.rb
+++ bin/main.rb
@@ -6,16 +6,16 @@ houses = { a1: "\s", a2: "\s", a3: "\s", b1: "\s", b2: "\s", b3: "\s", c1: "\s",
 
 puts "Game Start!\n"
 
-puts "Input player 1 name:\n"
+puts "Type, and then press enter, to chose player 1 name:\n"
 player1 = gets.chomp.capitalize
-puts "Input player 2 name:\n"
+puts "Type, and then press enter, to chose player 2 name:\n"
 player2 = gets.chomp.capitalize
 
 puts "INSTRUCTIONS:\n\n"
 puts "Tic-tac-toe is a very fun game!\n#{player1} gets the 'X' and #{player2} the 'O'\n\n"
 puts "The first one to connect three X's O's sequence is the Winner!\n\n"
 puts "If you want to quit the game, type 'quit' after you choose names\n\n"
-puts 'Select a cell by typing the row (letter) followed by the column (number)'
+puts 'Select a cell by typing the row (letter) followed by the column (number). Press enter to submit your selection.'
 puts "For example: 'a1'\n\n"
 
 puts "Here's your Tic-tac-toe game board!\n"
